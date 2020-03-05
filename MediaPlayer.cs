26800c1cf03fa82da43f8cd20092d35de4cb7257
ImaLuckyMan <Ima_Lucky_Man@yahoo.com>
2020-03-05 09:59:35 -0800

    Add project files.

---
+++ MediaPlayer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaPlayers
+{ 
+    public interface IStreamingDevice
+    {
+        public string DeviceName { get; set; }
+        public bool IsPlaying { get; set; } 
+        public bool IsPaused { get; set; }
+        bool Play();
+        void Pause();
+        void Ffwd();
+        void Rewind();
+        bool Stop();
+    }
+
+    class MediaPlayers
+    {
+    }
+
+    public class DVDPlayer : IStreamingDevice
+    {
+        public DVDPlayer()
+        {
+            DeviceName = "My DVD Player";
+            IsPlaying = false;
+            IsPaused = false;
+        }
+        
+        public string DeviceName { get; set; }
+
+        public bool IsPlaying { get; set;}
+        public bool IsPaused { get; set;}
+
+        public void Ffwd()
+        {
+            Console.WriteLine($"FFwd {DeviceName}");
+        }
+
+        public void Pause()
+        {
+            if(IsPlaying) Console.WriteLine($"{DeviceName} paused.");
+            else Console.WriteLine($"{DeviceName} stopped.");
+        }
+
+        public bool Play()
+        {
+            Console.WriteLine($"Play {DeviceName}");
+            IsPlaying = true;
+            IsPaused = false;
+            return IsPlaying;
+
+        }
+
+        public void Rewind()
+        {
+            Console.WriteLine($"Recind {DeviceName}");
+        }
+
+        public bool Stop()
+        {
+            Console.WriteLine($"Stop {DeviceName}");
+            IsPlaying = false;
+            return IsPlaying;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaPlayers
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("\n\tMedia Player Interface Demo!");
+
+            var mediaPlayer = new DVDPlayer();
+
+            Console.WriteLine($"\tstreaming from: {mediaPlayer.DeviceName} totally rocks!");
+            mediaPlayer.Pause();
+
+            while (true)
+            {
+                Console.WriteLine("\tPush 'Up-Arrow' to Play");
+                Console.WriteLine("\tPush '[Spacebar]' to Pause");
+                Console.WriteLine("\tPush 'R' to Rewind");
+                Console.WriteLine("\tPush 'F' to FFwd");
+                Console.WriteLine("\tPush 'S' to Stop");
+
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.F:
+                        mediaPlayer.Ffwd();
+                        break;
+                    case ConsoleKey.R:
+                        mediaPlayer.Rewind();
+                        break;
+                    case ConsoleKey.S:
+                        mediaPlayer.Stop();
+                        break;
+                    case ConsoleKey.UpArrow:
+                        mediaPlayer.Play();
+                        break;
+                    case ConsoleKey.Spacebar:
+                        mediaPlayer.Pause();
+                        break;
+                }
+
+            }
+        }
+    }
+}
